02bcbb467126020596a133d60ffcee7a295bddce
AnmolS1Z <71851700+AnmolS1Z@users.noreply.github.com>
2021-10-21 13:16:04 -0400

    Update Energy.py

--- Energy.py
+++ Energy.py
@@ -5,10 +5,10 @@ energyNeed = 2000
 def blockHeight():
     global energyHave, energyNeed
     if energyHave > energyNeed:
-        return 1
+        return 1 #drop weights
     else:
-        energyHave += 1000
-        blockHeight()
+        energyHave += 1000 #lift weights
+        blockHeight() #recursion until drop weights
 
 
 blockHeight()
