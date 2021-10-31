993d5cfada743089609caa48c6754c050f8981c8
alecmccue <85256756+alecmccue@users.noreply.github.com>
2021-10-31 10:58:25 -0400

    Update Energy.py

--- Energy.py
+++ Energy.py
@@ -1,13 +1,12 @@
-energyHave = 1000
-energyNeed = 2000
-
+energyHave = getEnergyReading() #Reading is pulled live from the Raspberry Pi
+energyNeed = getEnergyNeed() #Pre-determined number from the user
 
 def blockHeight():
     global energyHave, energyNeed
-    if energyHave > energyNeed:
+    if energyNeed > energyHave:
         return 1 #drop weights
     else:
-        energyHave += 1000 #lift weights
+        energyHave += liftWeight() #lift weights
         blockHeight() #recursion until drop weights
 
 
