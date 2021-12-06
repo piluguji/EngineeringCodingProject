2027b0278508f67a67f934b259d4eb6b6a1f022b
Anmol Sandhu <71851700+AnmolS1Z@users.noreply.github.com>
2021-12-06 16:32:01 -0500

    Update Battery.py

--- Battery.py
+++ Battery.py
@@ -4,10 +4,15 @@ import digitalio
 import board
 import adafruit_mcp3xxx.mcp3008 as MCP
 from adafruit_mcp3xxx.analog_in import AnalogIn
+import keyboard
 
 spi = busio.SPI(clock = board.SCK, MISO = board.MISO, MOSI = board.MOSI)
 cs = digitalio.DigitalInOut(board.D22)
 
 mcp = MCP.MCP3008(spi, cs)
-chan0 = AnalogIn(mcp, MCP.P0)
-print(str(chan0.voltage) + 'V')
+while True:
+  if keyboard.is_pressed():
+    break
+  chan0 = AnalogIn(mcp, MCP.P0)
+  print(str(chan0.voltage) + 'V')
+  time.sleep(0.2)
